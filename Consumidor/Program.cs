7429b166536608191038b0df523c87050d8d228a
David <luiz.oliveira2@dell.com>
2022-12-11 11:56:44 -0300

    Initial Commit

---
+++ Consumidor/Program.cs
@@ -0,0 +1,19 @@
+﻿using Confluent.Kafka;
+
+var config = new ConsumerConfig
+{
+    GroupId = "devio",
+    BootstrapServers = "localhost:9092"
+};
+
+using var consumer = new ConsumerBuilder<string, string>(config).Build();
+
+consumer.Subscribe("topico-teste");
+
+while (true)
+{
+    var result = consumer.Consume();
+
+    Console.WriteLine($"Mensagem: {result.Message.Key}-{result.Message.Value}");
+}
+
